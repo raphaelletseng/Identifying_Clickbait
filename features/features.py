d7f5cfc5888c29bf9e1bb2791229520c94804107
Miya Keilin <miya.keilin@gmail.com>
2020-12-07 15:42:54 -0800

    added new features

--- features/features.py
+++ features/features.py
@@ -32,31 +32,21 @@ from helper import process_text, sim_preprocess, is_contraction
 #   return (1-cosine)*100
   # print('Word Embedding method with a cosine distance asses that our two sentences are similar to',round((1-cosine)*100,2),'%')
 
+# i will organize this at some point so we don't call so many functions over and over again when we're calculating features - miya
+
 def word_count(text):
   return len(str(text).split(' '))
 
 def get_sentences(text):
   return nltk.tokenize.sent_tokenize(text)
 
-# doesn't work
-def get_paragraphs(text):
-  sent = ''
-  sentences = []
-  for c in text:
-      if c == '\n':
-          sentences.append(sent)
-          sent = ''
-      else:
-          sent += c
-  if sent != '':
-      sentences.append(sent)
-  return sentences
-
+# returns list of proper nouns (headline, content)
 def get_proper_nouns(text):
   tagged_sent = pos_tag(text.split())
   # [('word1', 'POS tag'),... ]
   return [word for word,pos in tagged_sent if pos == 'NNP']
 
+# returns average length of word (content)
 def avg_word_length(text):
   words = re.sub("[^a-zA-Z]", " ", text)
   words = words.split()
@@ -65,6 +55,7 @@ def avg_word_length(text):
     length += len(word)
   return length/len(words)
 
+# returns length of longest word (headline, content)
 def len_longest_word(text):
   words = re.sub("[^a-zA-Z]", " ", text)
   words = words.split()
@@ -74,13 +65,21 @@ def len_longest_word(text):
       max_length = len(word)
   return max_length
 
+# returns True if there is a question mark (headline)
 def qm_count(text):
-  count = 0
   for c in text:
     if c == '?':
-      count += 1
-  return count
+      return True
+  return False
 
+# returns True if there is an exclamation mark (headline)
+def em_count(text):
+  for c in text:
+    if c == '!':
+      return True
+  return False
+
+# returns number of contractions (headline, content)
 def num_contractions(text):
   words = text.split()
   count = 0
@@ -89,28 +88,26 @@ def num_contractions(text):
       count += 1
   return count
 
+# returns True if there is a digit (headline)
+def contains_number(text):
+  return any(c.isdigit() for c in text)
+
+# returns True if the first word is an adverb (headline)
+def contains_adverb(text):
+  tagged_sent = pos_tag(text.split())
+  if 'RB' in tagged_sent[0][1]:
+    return True
+  return False
+
+# returns average number of words per sentence (content)
+def avg_words_per_sentence(text):
+  return word_count(text)/len(get_sentences(text))
 
 article_data = 'articles1.csv'
 data = pd.read_csv(article_data,engine='python',usecols=['index','id','title','content'],nrows=100)
 data = data[data['content'].notna()]
 data = data[data['title'].notna()]
 
-# string = "can't eat without wasn't too aren't"
-# print(num_contractions(string))
-
-for i in range(data.shape[0]):
-  print(num_contractions(data.iloc[i]['content']))
-# print(qm_count(data.iloc[0]['content']))
-
-# sentences = get_sentences(data.iloc[0]['content'])
-# title = data.iloc[0]['title']
-# sim_score = 0
-# for sent in sentences:
-#   sim_score += cosine_dist_wordembedding_method(sim_preprocess(title),sim_preprocess(sent))
-# print(sim_score/len(sentences))
-# print(cosine_dist_wordembedding_method(sim_preprocess(title),sim_preprocess(data.iloc[0]['content'])))
-
-# print(len(get_proper_nouns(data.iloc[0]['content'])))
 
 # data['processed_content'] = data['content'].apply(process_text)
 # print(data.iloc[0]['processed_content'])
